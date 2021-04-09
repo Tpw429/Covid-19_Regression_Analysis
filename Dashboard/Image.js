35255c677373a5d86ae1edde05a692c593b68f63
tylerengalla <71526893+tylerengalla@users.noreply.github.com>
2021-04-08 22:32:03 -0500

    Working Drop Down Menu

---
+++ Dashboard/Image.js
@@ -0,0 +1,23 @@
+var images = {
+    GDP:
+        "Images/GDP_vs_VaccinationRate.png",
+    HDI:
+        "Images/HDI_vs_VaccinationRate.png",
+    HDI_Rank:
+        "Images/HDIRank_vs_VaccinationRate.png",
+    Life_Expectancy: 
+        "Images/LifeExpectancy_vs_VaccinationRate.png",
+    Population: 
+        "Images/Population_vs_VaccinationRate.png",
+    
+};
+var changeImage = function() {
+    const value = this.options[this.selectedIndex].value;
+    var imageURL = images[value];
+    document.getElementById("image").src = imageURL;
+};
+
+var imageList = document.getElementById("imageList");
+imageList.addEventListener("change", changeImage, false);
+
+document.getElementById("image").src = images["default"];
